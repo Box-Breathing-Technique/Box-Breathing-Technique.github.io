6da636985267bd206a2106fb5b19830ea64f80fc
Joshua Linehan <jlinehan@student.unimelb.edu.au>
2025-09-02 12:56:59 +1000

    removed construction components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,9 +6,6 @@
 
 import React from "react";
 import "./App.css";
-import WarningIcon from "../construction/WarningIcon";
-import WarningMessage from "../construction/WarningMessage";
-import ContactLink from "../construction/ContactLink";
 
 /** Master component for web app
  * @returns {React.ReactElement}
@@ -30,15 +27,7 @@ function App(): React.ReactElement {
             className="App"
             role="main"
             data-testid="app"
-        >
-            <WarningIcon
-                size={300}
-                weight={300}
-                color="grey"
-            />
-            <WarningMessage />
-            <ContactLink />
-        </div>
+        ></div>
     );
 }
 
