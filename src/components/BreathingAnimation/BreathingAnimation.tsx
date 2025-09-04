669c5299ed0a71cdaefb5f63c9a7abcf8c7fe7e1
Joshua Linehan <jlinehan@student.unimelb.edu.au>
2025-09-04 11:30:25 +1000

    parameterised animation and added easing

--- src/components/BreathingAnimation/BreathingAnimation.tsx
+++ src/components/BreathingAnimation/BreathingAnimation.tsx
@@ -4,20 +4,123 @@
  * @author Joshua Linehan
  */
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./BreathingAnimation.css";
 
+interface BreathingAnimationProps {
+    inDuration?: number;
+    holdInDuration?: number;
+    outDuration?: number;
+    holdOutDuration?: number;
+}
+
 /** Animation that demonstrates the box breathing technique
+ *
+ * @property {number?} [inDuration=4] How long the dot takes to move along the
+ * left side of the box, i.e. the breathe in duration
+ * @property {number?} [holdInDuration=4] How long the dot takes to move along
+ * the top of the box, i.e. the hold breath in duration
+ * @property {number?} [outDuration=4] How long the dot takes to move along the
+ * right side of the box, i.e. the breathe out duration
+ * @property {number?} [holdOutDuration=4] How long the dot takes to move along
+ * the bottom of the box, i.e. the hold breath out duration
  * @returns {React.ReactElement}
+ *
+ * @example
+ * // Default settings
+ * <BreathingAnimation />
+ *
+ * @example
+ * // Customized parameters
+ * <BreathingAnimation
+ *     inDuration={3}
+ *     holdInDuration={2}
+ *     outDuration={5}
+ *     holdOutDuration={2}
+ * />
  */
-function BreathingAnimation(): React.ReactElement {
+function BreathingAnimation({
+    inDuration = 4,
+    holdInDuration = 4,
+    outDuration = 4,
+    holdOutDuration = 4,
+}: BreathingAnimationProps): React.ReactElement {
+    // set animation duration
+    const animationDuration: number =
+        inDuration + holdInDuration + outDuration + holdOutDuration;
+    const breathingAnimationDotStyle: React.CSSProperties = {
+        "--animation-duration": `${animationDuration}s`,
+    } as React.CSSProperties;
+
+    // calculate percentages for animation
+    const inPercentage: number = inDuration / animationDuration;
+    const holdInPercentage: number =
+        holdInDuration / animationDuration + inPercentage;
+    const outPercentage: number =
+        outDuration / animationDuration + holdInPercentage;
+
+    // set animation
+    const animationTimingFunction: string = "cubic-bezier(0.4, 0.1, 0.6, 0.9)";
+    const dotRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const dot = dotRef.current;
+        if (!dot) return;
+        const keyframes: Keyframe[] = [
+            {
+                top: "100%",
+                left: "0%",
+                easing: animationTimingFunction,
+                offset: 0,
+            },
+            {
+                top: "0%",
+                left: "0%",
+                easing: animationTimingFunction,
+                offset: inPercentage,
+            },
+            {
+                top: "0%",
+                left: "100%",
+                easing: animationTimingFunction,
+                offset: holdInPercentage,
+            },
+            {
+                top: "100%",
+                left: "100%",
+                easing: animationTimingFunction,
+                offset: outPercentage,
+            },
+            {
+                top: "100%",
+                left: "0%",
+                easing: animationTimingFunction,
+                offset: 1,
+            },
+        ];
+
+        const animation = dot.animate(keyframes, {
+            duration: animationDuration * 1000,
+            iterations: Infinity,
+            easing: "linear",
+        });
+
+        // cleanup
+        return () => {
+            animation.cancel();
+        };
+    });
+
     return (
         <div
             className="BreathingAnimationContainer"
             data-testid="breathing-animation"
         >
             <div className="BreathingAnimationBox">
-                <div className="BreathingAnimationDot"></div>
+                <div
+                    className="BreathingAnimationDot"
+                    ref={dotRef}
+                    style={breathingAnimationDotStyle}
+                ></div>
             </div>
         </div>
     );
